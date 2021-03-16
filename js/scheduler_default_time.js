f9dcc8b2fe3ae367a9cb3f0ca23d994059227821
Jonathan Smith <jonathan1055@sandfordsolutions.com>
2021-03-16 09:05:25 +0000

    Add media test coverage for javascriptDefaultTime. Improve scheduler_default_time.js to make it run only once

--- js/scheduler_default_time.js
+++ js/scheduler_default_time.js
@@ -10,15 +10,23 @@
   /**
    * Provide default time if schedulerDefaultTime is set.
    *
-   * schedulerDefaultTime is defined in scheduler_form_node_form_alter when the
-   * user is allowed to enter just a date. The value need to be pre-filled here
+   * schedulerDefaultTime is defined in _scheduler_entity_form_alter when the
+   * user is allowed to enter just a date. The value needs to be pre-filled here
    * to avoid the browser validation 'please fill in this field' pop-up error
    * which is produced before the date widget valueCallback() can set the value.
    * @see https://www.drupal.org/project/scheduler/issues/2913829
    */
   Drupal.behaviors.setSchedulerDefaultTime = {
     attach: function (context) {
-      if (typeof drupalSettings.schedulerDefaultTime !== "undefined") {
+
+      // Drupal.behaviors are called many times per page. Using .once() adds the
+      // class onto the matched DOM element and uses this to prevent it running
+      // on subsequent calls.
+      const $default_time = $(context)
+        .find('#edit-scheduler-settings')
+        .once('default-time-done');
+
+      if ($default_time.length && typeof drupalSettings.schedulerDefaultTime !== "undefined") {
         var operations = ["publish", "unpublish"];
         operations.forEach(function (value) {
           var element = $("input#edit-" + value + "-on-0-value-time", context);
